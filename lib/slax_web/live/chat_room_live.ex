08b2cfba4d549682f4851ee82a95e08d57835263
Alexander Don <alexdon20022@gmail.com>
2025-04-21 00:39:24 +0300

    init commit

---
+++ lib/slax_web/live/chat_room_live.ex
@@ -0,0 +1,10 @@
+defmodule SlaxWeb.ChatRoomLive do
+  use SlaxWeb, :live_view
+
+  def render(assigns) do
+    ~H"""
+    <div>Welcome to the chat!</div>
+    <div>{2 + 2}</div>
+    """
+  end
+end
