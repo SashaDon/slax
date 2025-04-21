f0532306521b7041200f7d683dd9b7e0c02e5e8e
Alexander Don <alexdon20022@gmail.com>
2025-04-22 00:01:08 +0300

    added more logic to look test

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -7,10 +7,30 @@ defmodule SlaxWeb.ChatRoomLive do
 
     <%!-- normal ways of rendering elixir logic in heex --%>
     <div>{2 + 2}</div>
-    <div><%= 2 + 2 %></div>
+    <div>{2 + 2}</div>
 
     <%!-- without rendering --%>
     <div><% 2 + 2 %></div>
+
+    <%!-- loops --%>
+    <%!-- <ul>
+      <%= for number <- 1..10 do %>
+        <li>{number}</li>
+      <% end %>
+    </ul> --%>
+
+    <ul>
+      <li :for={number <- 1..10}>{number}</li>
+    </ul>
+
+    <%!-- conditionals --%>
+    <div id="person">
+      <%= if 15 > 10 do %>
+        <p>15 is greater than 10</p>
+      <% else %>
+        <p>15 is not greater than 10</p>
+      <% end %>
+    </div>
     """
   end
 end
