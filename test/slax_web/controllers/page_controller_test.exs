b4968b082cd42025d7d0e29a63756515b351dc4e
Alexander Don <alexdon20022@gmail.com>
2025-04-23 21:21:42 +0300

    updated the /home test case

--- test/slax_web/controllers/page_controller_test.exs
+++ test/slax_web/controllers/page_controller_test.exs
@@ -1,8 +1,8 @@
 defmodule SlaxWeb.PageControllerTest do
   use SlaxWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
+  test "GET /home", %{conn: conn} do
+    conn = get(conn, ~p"/home")
     assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
   end
 end
