0286eebf5c294393fbd3dfa0ea3fdf33ea086971
Alexander Don <alexdon20022@gmail.com>
2025-08-06 19:51:00 +0300

    show unread message counts

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -22,7 +22,12 @@ defmodule SlaxWeb.ChatRoomLive do
       <div class="mt-4 overflow-auto">
         <.toggler on_click={toggle_rooms()} dom_id="rooms-toggler" text="Rooms" />
         <div id="rooms-list">
-          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+          <.room_link
+            :for={{room, unread_count} <- @rooms}
+            room={room}
+            active={room.id == @room.id}
+            unread_count={unread_count}
+          />
 
           <div class="relative">
             <button
@@ -188,6 +193,7 @@ defmodule SlaxWeb.ChatRoomLive do
 
   attr :active, :boolean, required: true
   attr :room, Room, required: true
+  attr :unread_count, :integer, required: true
 
   defp room_link(assigns) do
     ~H"""
@@ -202,12 +208,13 @@ defmodule SlaxWeb.ChatRoomLive do
       <span class={["ml-2 leading-none", @active && "font-bold"]}>
         {@room.name}
       </span>
+      <.unread_message_counter count={@unread_count} />
     </.link>
     """
   end
 
   def mount(_params, _session, socket) do
-    rooms = Chat.list_joined_rooms(socket.assigns.current_user)
+    rooms = Chat.list_joined_rooms_with_unread_counts(socket.assigns.current_user)
     users = Accounts.list_users()
 
     timezone = get_connect_params(socket)["timezone"]
@@ -217,6 +224,8 @@ defmodule SlaxWeb.ChatRoomLive do
     end
 
     OnlineUsers.subscribe()
+
+    Enum.each(rooms, fn {room, _} -> Chat.subscribe_to_room(room) end)
 
     socket =
       socket
@@ -235,8 +244,6 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_params(params, _uri, socket) do
-    if socket.assigns[:room], do: Chat.unsubscribe_from_room(socket.assigns.room)
-
     room = params |> Map.fetch!("id") |> Chat.get_room!()
     last_read_at = Chat.get_last_read_at(room, socket.assigns.current_user)
 
@@ -245,7 +252,6 @@ defmodule SlaxWeb.ChatRoomLive do
       |> Chat.list_messages_in_room()
       |> maybe_insert_unread_marker(last_read_at)
 
-    Chat.subscribe_to_room(room)
     Chat.update_last_read_at(room, socket.assigns.current_user)
 
     socket =
@@ -257,6 +263,14 @@ defmodule SlaxWeb.ChatRoomLive do
       |> stream(:messages, messages, reset: true)
       |> assign_message_form(Chat.change_message(%Message{}))
       |> push_event("scroll_messages_to_bottom", %{})
+      |> update(:rooms, fn rooms ->
+        room_id = room.id
+
+        Enum.map(rooms, fn
+          {%Room{id: ^room_id} = room, _} -> {room, 0}
+          other -> other
+        end)
+      end)
 
     {:noreply, socket}
   end
@@ -288,7 +302,13 @@ defmodule SlaxWeb.ChatRoomLive do
     current_user = socket.assigns.current_user
     Chat.join_room!(socket.assigns.room, current_user)
     Chat.subscribe_to_room(socket.assigns.room)
-    socket = assign(socket, joined?: true, rooms: Chat.list_joined_rooms(current_user))
+    Chat.list_joined_rooms(current_user)
+
+    socket =
+      assign(socket,
+        joined?: true,
+        rooms: Chat.list_joined_rooms_with_unread_counts(current_user)
+      )
 
     {:noreply, socket}
   end
@@ -329,14 +349,29 @@ defmodule SlaxWeb.ChatRoomLive do
   end
 
   def handle_info({:new_message, message}, socket) do
-    if message.room_id == socket.assigns.room.id do
-      Chat.update_last_read_at(message.room, socket.assigns.current_user)
-    end
+    room = socket.assigns.room
 
     socket =
-      socket
-      |> stream_insert(:messages, message)
-      |> push_event("scroll_messages_to_bottom", %{})
+      cond do
+        message.room_id == room.id ->
+          Chat.update_last_read_at(room, socket.assigns.current_user)
+
+          socket
+          |> stream_insert(:messages, message)
+          |> push_event("scroll_messages_to_bottom", %{})
+
+        message.user_id != socket.assigns.current_user.id ->
+          update(socket, :rooms, fn rooms ->
+            # credo:disable-for-next-line Credo.Check.Refactor.Nesting
+            Enum.map(rooms, fn
+              {%Room{id: id} = room, count} when id == message.room_id -> {room, count + 1}
+              other -> other
+            end)
+          end)
+
+        true ->
+          socket
+      end
 
     {:noreply, socket}
   end
@@ -392,6 +427,19 @@ defmodule SlaxWeb.ChatRoomLive do
     message.inserted_at
     |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
+  end
+
+  attr :count, :integer, required: true
+
+  defp unread_message_counter(assigns) do
+    ~H"""
+    <span
+      :if={@count > 0}
+      class="flex items-center justify-center bg-blue-500 rounded-full font-medium h-5 px-2 ml-auto text-xs text-white"
+    >
+      {@count}
+    </span>
+    """
   end
 
   attr :user, User, required: true
